ca74cdb398234b6926d52da6e58b65b62f831e74
shekharjimkanpur <36585021+shekharjimkanpur@users.noreply.github.com>
2021-04-04 04:07:39 +0530

    added link in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,7 @@ $(document).ready( function () {
                     @endif
                     <div class='row'>
                     <div class='col-lg-2'>
-                    <button type="button" id='add_test' class='btn btn-md btn-primary'>Add New Test</button>
+                    <a type="button" id='add_test' href='/addtest' class='btn btn-md btn-primary'>Add New Test</a>
                     </div>
                     <div class='col-lg-2'>
                     <button type="button" class='btn btn-md btn-primary'>Add New User</button>
