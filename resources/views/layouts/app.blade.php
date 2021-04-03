18f8f416015269a35d4e72309892e669d2341822
shekharjimkanpur <36585021+shekharjimkanpur@users.noreply.github.com>
2021-04-04 03:47:56 +0530

    show testing list

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,7 @@ ob_start();
     <script src="https://code.jquery.com/jquery-latest.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
 </head>
 <body>
     <div id="app">
