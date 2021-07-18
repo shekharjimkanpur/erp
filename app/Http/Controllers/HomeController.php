7ae3842e5eddc3799f96f7927236a83ee8a0821d
Ravi Bhalla <ravi.bhalla97@gmail.com>
2021-07-18 16:59:41 +0530

    changes in homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,21 +30,27 @@ class HomeController extends Controller
 
     public function addClient(Request $request)
     {
+        
         $name =  $request->input('name');
         $phone =  $request->input('phone');
         $gst =  $request->input('gst');
         $email =  $request->input('email');
         $address =  $request->input('address');
-        //$users = DB::insert('select id, name from users');
-        $results = DB::insert( DB::raw("insert into erp_client(name,address,gst_no,email,number) values(:name,:address,:gst,:email,:phone)"), array(
-            'name' => $name,
-            'phone' => $phone,
-            'gst' => $gst,
-            'email' => $email,
-            'address' => $address,
-          ));
-        if($request)       
-            return "Successfully added!";
+        if($name!="" && $phone!="" && $gst!="" && $email!="" && $address!="" )
+        {
+            //$users = DB::insert('select id, name from users');
+            $results = DB::insert( DB::raw("insert into erp_client(name,address,gst_no,email,number) values(:name,:address,:gst,:email,:phone)"), array(
+                'name' => $name,
+                'phone' => $phone,
+                'gst' => $gst,
+                'email' => $email,
+                'address' => $address,
+            ));
+            if($request)       
+                return "Successfully added!";
+            else
+                return "Error! Please try again.";
+        }
         else
             return "Error! Please try again.";
     }
@@ -56,14 +62,18 @@ class HomeController extends Controller
         $name =  $request->input('name');
         $email =  $request->input('email');
         $pass =  $request->input('sec');
-        //$users = DB::insert('select id, name from users');
-        $results = DB::insert( DB::raw("insert into erp_department(name,email,password) values(:name,:email,:pass)"), array(
-            'name' => $name,
-            'email' => $email,
-            'pass' => $pass,
-          ));
-        if($request)       
-            return "Successfully added!";
+        if($name!="" && $email!="" && $pass!="" )
+        {
+            $results = DB::insert( DB::raw("insert into erp_department(name,email,password) values(:name,:email,:pass)"), array(
+                'name' => $name,
+                'email' => $email,
+                'pass' => $pass,
+            ));
+            if($request)       
+                return "Successfully added!";
+            else
+                return "Error! Please try again.";
+        }
         else
             return "Error! Please try again.";
     }
