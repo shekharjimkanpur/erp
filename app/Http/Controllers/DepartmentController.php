8f3cbdd56d3d0041d7764b14ed78fa6d0b5c17e3
Ravi Bhalla <ravi.bhalla97@gmail.com>
2021-09-25 17:56:50 +0530

    changes

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class DepartmentController extends Controller
 {
-    //
+    public function index()
+    {
+        $testing_data=$this->test_method_data();
+        return view('dept',['testing_data'=>$testing_data]);
+    }
+
+    public function test_method_data(){
+        $method=DB::SELECT('SELECT a.*,c.is_done as isdone,e.test_method as test_method ,d.id as test_param_id,d.name as test_param_name FROM `erp_test_product` a left join test_dept_util c on a.id=c.test_id left join test_params d on c.test_param_id=d.id left join erp_test_method e on c.test_method_id=e.id  where c.dept_id=1');
+        return $method;
+    }
 }
---
+++ resources/views/dept.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.app')
+
+@section('content')
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
+  <style>
+  body{
+      background-color:#F1F1F1 !important;
+  }
+  </style>
+  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
+<script>
+$(document).ready( function () {
+    $.noConflict();
+    $('#table_id').DataTable();
+} );
+</script>
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <!-- <div class="card-header">Testing List</div> -->
+
+                <div class="card-body">
+                    
+                
+                    
+                    <h4>Testing List</h4>
+                    <hr>
+<div class='row'>
+<div class='col-lg-12'>
+                    <table id="table_id" class="display">
+    <thead>
+        <tr>
+            
+            <th>Test Id</th>
+            
+            <th>Test Parameter Name</th>
+            
+            <th>Created on</th>
+            <th>Testing status</th>
+            <th>actions</th>
+
+        </tr>
+    </thead>
+    <tbody>
+    @foreach ($testing_data as  $tests)
+        <tr>
+            <td>{{$tests->test_gen_id}}</td>
+            <td>{{$tests->test_param_name}}</td>
+            
+            
+            <td>{{$tests->booking_date}}</td>
+            
+            <td>@if($tests->isdone=='1') Completed @else Pending @endif</td>
+            
+            <td>  
+            <a type="button" id='edit_test' href='edit_test?id={{ $tests->id }}'  class='btn btn-sm btn-primary'>Add Params</a>
+            <!-- <button type="button" class='btn btn-sm btn-primary'>add</button> -->
+            <!-- <button type="button" id='delete_test' class='btn btn-sm btn-danger'>Delete</button> -->
+                     </td>
+        </tr>
+        @endforeach
+      </tbody>
+</table>
+
+</div>
+</div>
+
+
+</section>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+ 
+
+   
+@endsection
